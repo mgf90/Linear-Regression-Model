d5807fb7999d067220047fcc77b7c93d08939f50
Gavin Farmer <mgavinfarmer@gmail.com>
2022-05-25 11:34:32 -0400

    predict passer rating added

--- main.py
+++ main.py
@@ -4,72 +4,72 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 import datetime
 
+# fits model and displays scatter plot
+def fit_model(x, y, xlabel, ylabel):
+    x = x.reshape(-1, 1)
+    y = y.reshape(-1, 1)
+
+    model = linear_model.LinearRegression()
+    model.fit(x, y)
+
+    y_predict = model.predict(x)
+
+    plt.scatter(x, y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title('NFL QBs 2010-2021')
+
+    plt.plot(x, y_predict, color='r')
+    plt.show()
 
 class main:
 
     qbs = pd.read_csv("qb_stats_2010_2021.csv")
     exit_program = False
+
     print('\nWelcome to Advanced Football Analytics!\n'
           'Using data from 2010-2021, this model can predict NFL QB stats\n')
 
     while not exit_program:
 
-        print('Select the following statistics to see their correlation:\n\n'
-              '1 - Completion Percentage and Yards Per Attempt\n'
-              '2 - Rushing Yards Per Game and Passer Rating\n'
-              '3 - Passing Attempts Per Game and Passer Rating\n'
+        print('MAIN MENU\n\n'
+              '1 - Completion Percentage and Yards Per Attempt Model\n'
+              '2 - Rushing Yards Per Game and Passer Rating Model\n'
+              '3 - Passing Attempts Per Game and Passer Rating Model\n'
               '4 - Predict Passer Rating\n'
               '5 - Exit Program\n\n'
               'Enter the number corresponding to your selection: ')
 
         choice = int(input())
 
+        # selects Completion Percentage model
         if choice == 1:
 
             x = qbs['CmpP'].values
             y = qbs['Y/A'].values
+            xlabel = 'Career Completion Percentage'
+            ylabel = 'Career Yards Per Attempt'
 
-            x = x.reshape(-1, 1)
-            y = y.reshape(-1, 1)
+            fit_model(x, y, xlabel, ylabel)
 
-            model = linear_model.LinearRegression()
-            model.fit(x, y)
-            y_predict = model.predict(x)
-
-            plt.scatter(x, y)
-            plt.xlabel('Career Completion Percentage')
-            plt.ylabel('Career Yards Per Attempt')
-            plt.title('NFL QBs 2010-2021')
-
-            plt.plot(x, y_predict, color='r')
-            plt.show()
-
+        # selects Rushing Yards Per Game model
         elif choice == 2:
 
             x = qbs['RushYardsPerGame'].values
             y = qbs['Rate'].values
+            xlabel = 'Career Rushing Yards Per Game'
+            ylabel = 'Career Passer Rating'
 
-            x = x.reshape(-1, 1)
-            y = y.reshape(-1, 1)
+            fit_model(x, y, xlabel, ylabel)
 
-            model = linear_model.LinearRegression()
-            model.fit(x, y)
-            y_predict = model.predict(x)
-
-            plt.scatter(x, y)
-            plt.xlabel('Career Rushing Yards Per Game')
-            plt.ylabel('Career Passer Rating')
-            plt.title('NFL QBs 2010-2021')
-
-            plt.plot(x, y_predict, color='r')
-            plt.show()
-
+        # selects Passing Attempts Per Game model
         if choice == 3:
 
             x = qbs['Pass_Att'].values
             g = qbs['G'].values
             apg = []
 
+            # calculates passing attempts per game
             for i in range(0, len(x)):
                 apg.append(x[i] / g[i])
 
@@ -77,25 +77,31 @@ class main:
             x = np.array(x)
             y = qbs['Rate'].values
 
-            x = x.reshape(-1, 1)
-            y = y.reshape(-1, 1)
+            xlabel = 'Career Passing Attempts Per Game'
+            ylabel = 'Career Passer Rating'
+
+            fit_model(x, y, xlabel, ylabel)
+
+        # selects passer rating prediction
+        elif choice == 4:
 
             model = linear_model.LinearRegression()
-            model.fit(x, y)
-            y_predict = model.predict(x)
+            model.fit(qbs[['CmpP']], qbs.Rate)
+            inp = float(input('Enter a career completion percentage. Just type the number: '))
+            inp = [[inp]]
+            y_predict = model.predict(inp)
 
-            plt.scatter(x, y)
-            plt.xlabel('Career Passing Attempts Per Game')
-            plt.ylabel('Career Passer Rating')
-            plt.title('NFL QBs 2010-2021')
+            inp = str(inp[0][0])
+            print(f'A player with a completion percentage of {inp}% is predicted to have a {y_predict[0]} '
+                  f'passer rating.\n')
 
-            plt.plot(x, y_predict, color='r')
-            plt.show()
 
-        # elif choice == 4:
-        #
-        # elif choice == 5:
+        # exits the program
+        elif choice == 5:
 
+            exit_program = True
+
+        # prints error message and writes it to error log. Returns to main menu
         else:
 
             print('Invalid input. Please try again')
